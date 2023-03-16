02316fd7016e36af82191cc903c6170c67d2dc61
Synectome <jessedgb@gmail.com>
2023-03-16 15:19:59 -0400

    starting of handRank class

--- handRank.py
+++ handRank.py
@@ -1,7 +1,7 @@
 # take 7 cards and determine the best hand that can be made from 
 # at least 1 card from plyr hand, and 3 or 4 cards from the table
 # then return a hand rank
-
+import cardDeck as cd
 # hand ranks:
 # 1: high card = [hc, 0, (c1, c2, c3, c4, c5)]
 # 2: pair = [p, 1, (c1, c2, c3, c4, c5)]
@@ -16,6 +16,7 @@
 
 def hand_value(player_cards, table_cards):
     # check for royal flush
+
     # check for straight flush
     # check for four of a kind
     # check for full house
@@ -27,14 +28,44 @@ def hand_value(player_cards, table_cards):
     # check for high card
     pass
 
-def straight():
-    # check for straight
-    # if straight, return True
-    # else return False
-    pass
+def is_straight(player_cards, table_cards):
+    rankic = {}
+    for card in player_cards:
+        rank = cd.get_rank(card)
+        if rank in rankic.keys():
+            rankic[rank] += 1
+        else:
+            rankic[rank] = 1
+    for card in table_cards:
+        rank = cd.get_rank(card)
+        if rank in rankic.keys():
+            rankic[rank] += 1
+        else:
+            rankic[rank] = 1
+    
 
-def flush():
-    # check for flush
-    # if flush, return True
-    # else return False
-    pass
+"""Returns true if there is a flush involving at least one card of the players hand.
+    Does not check for ranking of flush."""
+def is_flush(player_cards, table_cards):
+    suitic = {}
+    for card in player_cards:
+        suit = cd.get_suit(card)
+        if suit in suitic.keys():
+            suitic[suit] += 1
+        else:
+            suitic[suit] = 1
+    for card in table_cards:
+        suit = cd.get_suit(card)
+        if suit in suitic.keys():
+            suitic[suit] += 1
+        else:
+            suitic[suit] = 1
+    for suit, count in suitic.items():
+        if count >= 5 and (cd.get_suit(player_cards[0]) == suit or cd.get_suit(player_cards[1]) == suit):
+            return True
+    return False
+
+    
+
+
+    
